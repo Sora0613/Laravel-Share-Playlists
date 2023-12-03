e58072b06988a613739b438c8d645f5234ef151c
Sora0613 <sora0613.contact@gmail.com>
2023-12-03 16:48:37 +0900

    update. Seeder実行するたびに初期化を行うように。

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::truncate();
         User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
