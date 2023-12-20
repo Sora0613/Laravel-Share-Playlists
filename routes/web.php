022a93b15cb8285fa1229cb3dc8ad8af70280125
Sora0613 <sora0613.contact@gmail.com>
2023-12-20 19:21:56 +0900

    update. 以下のページの変更によるルートの設定。 プレイリストの一覧を出す単体のページを作った プレイリストの曲の削除、追加を行えるようにした。

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,14 @@ Route::middleware(['auth'])->group(function () {
     /*通常プレイリスト*/
 
     Route::get('/home', [App\Http\Controllers\PlaylistsController::class, 'index'])->name('index'); //表示
+    Route::get('/playlist/all', [App\Http\Controllers\PlaylistsController::class, 'showAllPlaylists'])->name('playlist.all'); //表示
     Route::get('/playlist/create', [App\Http\Controllers\PlaylistsController::class, 'playlistCreate'])->name('playlist.create'); //表示
     Route::post('/playlist/create', [App\Http\Controllers\PlaylistsController::class, 'playlistStore'])->name('playlist.store'); //登録
 
-    Route::get('/playlist/{playlist_id}', [App\Http\Controllers\PlaylistsController::class, 'playlistShow'])->name('playlist.show'); //表示
+    Route::post('/playlist/song/add', [App\Http\Controllers\PlaylistsController::class, 'playlistSongAdd'])->name('playlist.song.add'); //登録
+    Route::delete('/playlist/song/delete/{song_id}', [App\Http\Controllers\PlaylistsController::class, 'playlistSongDelete'])->name('playlist.song.delete'); //登録
+
+    //Route::get('/playlist/{playlist_id}', [App\Http\Controllers\PlaylistsController::class, 'playlistShow'])->name('playlist.show'); //表示
 
     /* iTunesここから */
 
@@ -51,3 +55,5 @@ Route::middleware(['auth'])->group(function () {
     /* Spotifyここまで */
 
 });
+
+Route::get('/playlist/{playlist_id}', [App\Http\Controllers\PlaylistsController::class, 'playlistShow'])->name('playlist.show'); //ログインしていない人にも表示を行いたいため、Authから除外。
