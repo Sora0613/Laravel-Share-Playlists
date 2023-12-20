b12ac40724585de2616b9423cf56d3e41cf8eb36
Sora0613 <sora0613.contact@gmail.com>
2023-12-20 19:22:42 +0900

    update. プレイリストの楽曲を削除できるようにした。

--- resources/views/playlists/show.blade.php
+++ resources/views/playlists/show.blade.php
@@ -11,15 +11,53 @@
 <div class="container">
     <a href="{{ route('index') }}" class="top-link">Back to Top</a>
     <h1>Detail of your playlist.</h1>
-    @isset($message)
-        <p>{{ $message }}</p>
-    @endisset
+    @if(session('message'))
+        <p>{{ session('message') }}</p>
+    @endif
 
     <div class="playlist-list">
         <div class="playlist-item">
             @isset($playlist)
                 <h2>Playlist Name : {{ $playlist->playlist_name }}</h2>
                 <p>Playlist Description : {{ $playlist->playlist_description }}</p>
+
+
+                @isset($songs)
+                    <div class="result-form">
+                        <table>
+                            <thead>
+                            <tr>
+                                <th>Artwork</th>
+                                <th>Artist</th>
+                                <th>Song Title</th>
+                                <th>Album Name</th>
+                                @if($playlist->user_id === Auth::user()->id)
+                                    <th>Edit</th>
+                                @endif
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($songs as $song)
+                                <tr>
+                                    <td><img src="{{ $song->artwork_url }}" alt="song artwork" width="100" height="100"></td>
+                                    <td>{{ $song->artist_name }}</td>
+                                    <td>{{ $song->song_name }}</td>
+                                    <td>{{ $song->album_name }}</td>
+                                    @if($playlist->user_id === Auth::user()->id)
+                                        <td>
+                                            <form action="{{ route('playlist.song.delete', ['song_id' => $song->id ]) }}" method="POST">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit">Delete</button>
+                                            </form>
+                                        </td>
+                                    @endif
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                @endisset
             @endisset
         </div>
     </div>
