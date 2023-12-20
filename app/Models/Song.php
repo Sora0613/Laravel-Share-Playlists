e7c4fd94e5f6fa25f3aaf851196aab3c72309c5b
Sora0613 <sora0613.contact@gmail.com>
2023-12-20 19:24:47 +0900

    update. プレイリストへの楽曲追加。削除を行うための変更

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Song extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'song_name','artist_name','album_name','artwork_url','playlist_id'
+    ];
 }
